5e56f99e416eafa98e1409055eca822d0626ef91
alexd <alexdidenko7@gmail.com>
2022-05-30 02:23:34 +0300

    styles added

--- add-product.php
+++ add-product.php
@@ -20,15 +20,137 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Product Add</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet"> 
     <style>
+        body {
+            font-family: 'Ubuntu', sans-serif;
+            padding: 0;
+            margin: 0;
+            color: #6e5227;
+            font-size: 1vw;
+            background-color: #fff3e0;
+        }
+        div.body {
+            width: 80%;
+            min-height: 100vh;
+            margin-left: 10%;
+            margin-right: 10%;
+            background-color: #f5d99a;
 
+        }
+        span.head {
+            font-size: 3vw;
+            margin-left: 1vw;
+        }
+        button {
+            font-family: 'Ubuntu', sans-serif;
+            height: 2.4vw;
+            border: 0.15vw solid #6e5227;
+            border-radius: 0.7vw;
+            display: inline-block;
+            padding: 0.1vw 0.5vw; 
+            text-decoration: none; 
+            color: #6e5227;
+            background-color: #fff3e0;
+            font-size: 1.8vw;
+            cursor: pointer;
+        }
+        div.buttons {
+            position: absolute;
+            right: 11%;
+            top: 0.7vw;
+        }
+        hr {
+            width: 96%;
+            margin-left: 2%;
+            margin-right: 2%;
+            margin-top: 0.3vw;
+            margin-bottom: 0.7vw;
+            color: #6e5227;
+        }
+        table {
+            width:40%;
+            margin-left: 25%;
+            margin-right: 35%;
+            margin-bottom: 0;
+        }
+        td {
+            text-align: end;
+            width: 50%;
+        }
+        input.value, select.value {
+            background-color: #fff3e0;
+            border: 0.15vw solid #6e5227;
+            border-radius: 0.3vw;
+            width: 100%;
+            font-family: 'Ubuntu', sans-serif;
+            font-size: 1vw;
+            margin: 0;
+        }
+        select.value {
+            width: 102.5%;
+            cursor: pointer;
+        }
+        option {
+            font-size: 0.9vw;
+        }
     </style>
-    <script
-			  src="https://code.jquery.com/jquery-3.6.0.js"
-			  integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
-			  crossorigin="anonymous"></script>
-    <script>
-        
+</head>
+<body onload="properties_output()">
+    <div class="body">
+        <span class="head">Product Add</span>
+        <div class="buttons">
+            <a href="index.php"><button class="cancel">CANCEL</button></a>
+            <button class="save" onclick="validate()">Save</button>
+        </div>
+        <hr>
+        <form action="index.php" method="post" id="product_form">
+            <?php  
+                if( !$connection ) {
+                    echo 'Failed to connect to database!<br>';
+                    echo mysqli_connect_error();
+                    exit();
+                }
+            ?>
+            <table>
+                <tr>
+                    <td>SKU:</td>
+                    <td><input type="text" name="sku" class="value" id="sku" required ></td>
+                </tr>
+                <tr>
+                    <td>Name:</td>
+                    <td><input type="text" name="name" class="value" id="name" required ></td>
+                </tr>
+                <tr>
+                    <td>Price ($):</td>
+                    <td><input type="text" name="price" class="value" id="price" required ></td>
+                </tr>
+                <tr>
+                    <td>Type switcher:</td>
+                    <td>
+                        <select name="category" class="value" id="productType" required onchange="properties_output()">
+                            <?php
+                                $result = mysqli_query($connection, 'SELECT * FROM `categories`;');
+                                while( $array = mysqli_fetch_assoc($result) ) {
+                                    echo '<option value="' . $array['category_id'] . '" id="' . $array['category_name'] . '"';
+                                    echo '>' . $array['category_name'] . '</option>';
+                                }
+                            ?>
+                        </select>
+                    </td>
+                </tr>
+            </table>
+            <table id="properties">
+            </table>
+            <?php
+                mysqli_close($connection);
+            ?>
+        </form>
+    </div>
+</body>
+<script>
         function validate_field(field_name) {
             if (!isFinite(document.getElementById(field_name).value)) {
                 alert('Warning, ' + field_name + ' should be numeric!');
@@ -77,92 +199,31 @@
                     }
                     break;
                 case '3':
-                    if (validate_field('height') || validate_field('width') || validate_field('length')) {
+                    if (!validate_field('height') || !validate_field('width') || !validate_field('length')) {
                         return false;
                     }
                     break;
             }
+            document.getElementById('product_form').submit();
         }
         function properties_output() {
-            /*if (document.getElementById('properties') != null) {
-                document.getElementById('properties').remove();
-            }
-            let where = document.getElementById('insert_after_this');
-            let table = document.createElement('table');
-            table.setAttribute('id', 'properties');*/
             let where = document.getElementById('properties');
             switch(document.getElementById('productType').value) {
                 case '1':
-                    where.innerHTML = '<tr><td>Size (MB):</td><td><input type="text" name="size" id="size" required></td></tr>';
+                    where.innerHTML = '<tr><td>Size (MB):</td><td><input type="text" name="size" class="value" id="size" required></td></tr>';
                     break;
                 case '2':
-                    where.innerHTML = '<tr><td>Weight (KG):</td><td><input type="text" name="weight" id="weight" required></td></tr>';
+                    where.innerHTML = '<tr><td>Weight (KG):</td><td><input type="text" name="weight" class="value" id="weight" required></td></tr>';
                     break;
                 case '3':
-                    where.innerHTML = '<tr><td>Height (CM):</td><td><input type="text" name="height" id="height" required></td></tr>' +
-                    '<tr><td>Width (CM):</td><td><input type="text" name="width" id="width" required></td></tr>' +
-                    '<tr><td>Length (CM):</td><td><input type="text" name="length" id="length" required></td></tr>';
+                    where.innerHTML = '<tr><td>Height (CM):</td><td><input type="text" name="height" class="value" id="height" required></td></tr>' +
+                    '<tr><td>Width (CM):</td><td><input type="text" name="width" class="value" id="width" required></td></tr>' +
+                    '<tr><td>Length (CM):</td><td><input type="text" name="length" class="value" id="length" required></td></tr>';
                     break;
             }
             return;
         }
         
     </script>
-</head>
-<body>
-    <h1>Product Add</h1>
-    <a href="index.php"><button class="cancel">CANCEL</button></a>
-    <button class="save" form="product_form" onclick="validate()">Save</button>
-    <form action="index.php" method="post" id="product_form">
-        <hr color='black'>
-        <?php  
-            if( !$connection ) {
-                echo 'Failed to connect to database!<br>';
-                echo mysqli_connect_error();
-                exit();
-            }
-        ?>
-        <table>
-            <tr>
-                <td>SKU:</td>
-                <td><input type="text" name="sku" id="sku" required ></td>
-            </tr>
-            <tr>
-                <td>Name:</td>
-                <td><input type="text" name="name" id="name" required ></td>
-            </tr>
-            <tr>
-                <td>Price ($):</td>
-                <td><input type="text" name="price" id="price" required ></td>
-            </tr>
-            <tr>
-                <td>Type switcher:</td>
-                <td>
-                    <select name="category" id="productType" required onchange="properties_output()">
-                        <?php
-                            $result = mysqli_query($connection, 'SELECT * FROM `categories`;');
-                            while( $array = mysqli_fetch_assoc($result) ) {
-                                echo '<option value="' . $array['category_id'] . '" id="' . $array['category_name'] . '"';
-                                if (isset($_POST['category']) and $_POST['category'] == $array['category_id']) echo ' selected';
-                                echo '>' . $array['category_name'] . '</option>';
-                            }
-                        ?>
-                    </select>
-                </td>
-            </tr>
-        </table>
-        <table id="properties">
-            <tr>
-                <td>Size (MB):</td>
-                <td>
-                    <input type="text" name="size" id="size" required>
-                </td>
-            </tr>
-        </table>
-        <?php
-            mysqli_close($connection);
-        ?>
-    </form>
-</body>
 </html>
 
--- index.php
+++ index.php
@@ -20,94 +20,164 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Product List</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet"> 
     <style>
-        
+        body {
+            font-family: 'Ubuntu', sans-serif;
+            padding: 0;
+            margin: 0;
+            color: #6e5227;
+            font-size: 1vw;
+            background-color: #fff3e0;
+        }
+        div.body {
+            width: 80%;
+            min-height: 100vh;
+            margin-left: 10%;
+            margin-right: 10%;
+            background-color: #f5d99a;
+
+        }
+        span.head {
+            font-size: 3vw;
+            margin-left: 1vw;
+        }
+        button {
+            font-family: 'Ubuntu', sans-serif;
+            height: 2.4vw;
+            border: 0.15vw solid #6e5227;
+            border-radius: 0.7vw;
+            display: inline-block;
+            padding: 0.1vw 0.5vw; 
+            text-decoration: none; 
+            color: #6e5227;
+            background-color: #fff3e0;
+            font-size: 1.8vw;
+            cursor: pointer;
+        }
+        div.buttons {
+            position: absolute;
+            right: 11%;
+            top: 0.7vw
+        }
+        hr {
+            width: 96%;
+            margin-left: 2%;
+            margin-right: 2%;
+            margin-top: 0.3vw;
+            margin-bottom: 0.7vw;
+            color: #6e5227;
+        }
+        table {
+            width:98%;
+            margin-left: 1%;
+            margin-right: 1%;
+            margin-bottom: 0.5vw;
+        }
+        td {
+            width: 20%;
+            background-color: #fff3e0;
+            border: 0.15vw solid #6e5227;
+            border-radius: 0.7vw;
+        }
+        div.table_text {
+            text-align: center;
+        }
+        input.delete-checkbox {
+            width: 1vw;
+        }
     </style>
 </head>
 <body>
-    <h1>Product list</h1>
-    <a href="add-product.php"><button class="add">ADD"</button></a>
-    <button form="products" type="submit" name="delete" class="mass_delete">MASS DELETE</button>
-    <form action="index.php" method="POST">
-        <hr color='black'>
-        <?php  
-            if( !$connection ) {
-                echo 'Failed to connect to database!<br>';
-                echo mysqli_connect_error();
-                exit();
-            }
-
-            if (isset($_POST['delete'])) {
-                for( $i = 1; $i <= $_POST['count']; $i++ ) {
-                    if (isset($_POST['product' . $i])) {
-                        mysqli_query($connection, 'DELETE FROM products WHERE `products`.`sku` = "' . $_POST['product' . $i] . '"');
+    <div class="body">
+        <span class="head">Product list</span>
+        <div class="buttons">
+            <a href="add-product.php"><button class="add">ADD</button></a>
+            <button form="products" type="submit" name="delete" class="mass_delete">MASS DELETE</button>
+        </div>
+        <hr>
+        <form action="index.php" method="POST" id="products">
+            <?php  
+                if( !$connection ) {
+                    echo 'Failed to connect to database!<br>';
+                    echo mysqli_connect_error();
+                    exit();
+                }
+                
+                if (isset($_POST['delete'])) {
+                    for( $i = 1; $i <= $_POST['count']; $i++ ) {
+                        if (isset($_POST['product' . $i])) {
+                            mysqli_query($connection, 'DELETE FROM products WHERE `products`.`sku` = "' . $_POST['product' . $i] . '"');
+                        }
                     }
                 }
-            }
 
-            if (isset($_POST['sku'])) {
-                $categories_query = mysqli_query($connection, 'SELECT * FROM `properties` WHERE `category_id` = ' . $_POST['category'] . ';');
+                if (isset($_POST['sku'])) {
+                    $categories_query = mysqli_query($connection, 'SELECT * FROM `properties` WHERE `category_id` = ' . $_POST['category'] . ';');
 
-                mysqli_query($connection, 'INSERT INTO `products` (`sku`, `name`, `price`, `category_id`) VALUES ("'
-                . $_POST['sku'] .'", "'
-                . $_POST['name'] . '", "'
-                . $_POST['price'] . '", "'
-                . $_POST['category'] . '");');
-                while( $categories = mysqli_fetch_assoc($categories_query) ) {
-                    mysqli_query($connection, 'INSERT INTO `properties_values` (`property_id`, `property_value`, `sku`) VALUES ("'
-                    . $categories['property_id'] . '", "'
-                    . $_POST[$categories['property_name']] . '", "'
-                    . $_POST['sku'] . '");');
+                    mysqli_query($connection, 'INSERT INTO `products` (`sku`, `name`, `price`, `category_id`) VALUES ("'
+                    . $_POST['sku'] .'", "'
+                    . $_POST['name'] . '", "'
+                    . $_POST['price'] . '", "'
+                    . $_POST['category'] . '");');
+                    while( $categories = mysqli_fetch_assoc($categories_query) ) {
+                        mysqli_query($connection, 'INSERT INTO `properties_values` (`property_id`, `property_value`, `sku`) VALUES ("'
+                        . $categories['property_id'] . '", "'
+                        . $_POST[$categories['property_name']] . '", "'
+                        . $_POST['sku'] . '");');
+                    }
                 }
-            }
 
-            if( !($result = mysqli_query($connection, 'SELECT * FROM `products` INNER JOIN `categories` ON (products.category_id = categories.category_id);')) ) {
-                echo 'Product list is empty!';
-                exit();
-            }
+                if( !($result = mysqli_query($connection, 'SELECT * FROM `products` INNER JOIN `categories` ON (products.category_id = categories.category_id);')) ) {
+                    echo 'Product list is empty!';
+                    exit();
+                }
 
-            $counter = 0;
-            $products_per_line = 5;
-            echo '<table>';
-            while( $array = mysqli_fetch_assoc($result) ) {
-                if( $counter % $products_per_line == 0 ) {
-                    echo '</tr><tr>';
-                }
-                $counter++;
-                echo '<td>';
-                echo '<input type="checkbox" class="delete-checkbox" name="product' . $counter . '" value="' . $array['sku'] . '"><br>';
-                echo 'SKU: ' . $array['sku'] . '<br>';
-                echo $array['name'] . '<br>';
-                echo $array['price'] . '$<br>';
-                $properties_result = mysqli_query($connection, 'SELECT * FROM `properties_values` WHERE `sku`="' . $array['sku'] . '";');
-                $properties_array = mysqli_fetch_assoc($properties_result);
-                if( $array['category_name'] == 'DVD' ) {
-                    echo 'Size: ' . $properties_array['property_value'] . 'MB<br>';
-                }
-                if( $array['category_name'] == 'Book' ) {
-                    echo 'Weight: ' . $properties_array['property_value'] . 'KG<br>';
-                }
-                if( $array['category_name'] == 'Furniture' ) {
-                    $height = $properties_array['property_value'];
+                $counter = 0;
+                $products_per_line = 5;
+                echo '<table>';
+                while( $array = mysqli_fetch_assoc($result) ) {
+                    if( $counter % $products_per_line == 0 ) {
+                        echo '</tr><tr>';
+                    }
+                    $counter++;
+                    echo '<td>';
+                    echo '<input type="checkbox" class="delete-checkbox" name="product' . $counter . '" value="' . $array['sku'] . '"><br><div class="table_text">';
+                    echo 'SKU: ' . $array['sku'] . '<br>';
+                    echo $array['name'] . '<br>';
+                    echo $array['price'] . '$<br>';
+                    $properties_result = mysqli_query($connection, 'SELECT * FROM `properties_values` WHERE `sku`="' . $array['sku'] . '";');
                     $properties_array = mysqli_fetch_assoc($properties_result);
-                    $width = $properties_array['property_value'];
-                    $properties_array = mysqli_fetch_assoc($properties_result);
-                    $length = $properties_array['property_value'];
-                    echo 'Dismensions: ' . $height . 'x' . $width . 'x' . $length . '<br>';
+                    if( $array['category_name'] == 'DVD' ) {
+                        echo 'Size: ' . $properties_array['property_value'] . 'MB<br>';
+                    }
+                    if( $array['category_name'] == 'Book' ) {
+                        echo 'Weight: ' . $properties_array['property_value'] . 'KG<br>';
+                    }
+                    if( $array['category_name'] == 'Furniture' ) {
+                        $height = $properties_array['property_value'];
+                        $properties_array = mysqli_fetch_assoc($properties_result);
+                        $width = $properties_array['property_value'];
+                        $properties_array = mysqli_fetch_assoc($properties_result);
+                        $length = $properties_array['property_value'];
+                        echo 'Dismensions: ' . $height . 'x' . $width . 'x' . $length . '<br>';
+                    }
+                    echo '</div></td>';
                 }
-                echo '</td>';
-            }
-            echo '</tr></table>';
-            echo '<input type="hidden" name="count" value="' . $counter . '">';
+                echo '</tr></table>';
+                echo '<input type="hidden" name="count" value="' . $counter . '">';
 
-            mysqli_close($connection);
+                mysqli_close($connection);
 
-            spl_autoload_register(function ($class_name) {
-                include 'Models\\' . $class_name . '.php';
-            }, true, false);
+                spl_autoload_register(function ($class_name) {
+                    include 'Models\\' . $class_name . '.php';
+                }, true, false);
 
-        ?>
-    </form>
+            ?>
+        </form>
+    </div>
 </body>
 </html>
 
